23668f436bc6d36bf19b5e53d384fcbb6c6c9dc3
gabriel palhano <gabrielpalhano.dev@gmail.com>
2022-11-27 20:45:35 -0300

    Contador de passos (exercício pt-3)
    JS completo!

--- script.js
+++ script.js
@@ -4,20 +4,29 @@ function clicou() {
     var passod = document.getElementById('txtpasso')
     var res = document.querySelector('div#res')
 
-        if (inid.value.length == 0 || fimd.value.length == 0) {
-            window.alert('[ERRO] Verifique os dados e tente novamente!')
-        } else if (passod.value.lenght == 0) {
-            window.alert('O campo PASSO não foi preenchido, portanto será realizada a contagem normal de: 1 por vez!');
-            res.innerHTML = 'Contando'
+        if (inid.value.length == 0 || fimd.value.length == 0 || passod.value.length == 0) {
+            res.innerHTML = 'Impossível contar!'
+            window.alert('[ERRO] Verifique os dados e tente novamente!');
+            
+        } else {
+            res.innerHTML = 'Contando:'
             var i = Number(inid.value);
             var f = Number(fimd.value);
             var p = Number(passod.value);
-                for(let c = i; c <= f; c++) {
-                    res.innerHTML = `${c}`
-                }
-        }
-  
 
+            if (p <= 0) {
+                window.alert('Passo inválido! considerando passo igual a: 1.');
+                p = 1;
+            }
 
-        
+                if (i < f) {
+                    for(let c = i; c <= f; c += p) {
+                        res.innerHTML += ` ${c} \u{1F449}`
+                    }
+                } else {
+                    for(let c = i; c >= f; c -= p) {
+                        res.innerHTML += `${c} \u{1f449}`
+                    } 
+                }  res.innerHTML += `\u{1F3C1}`
+        }      
 }
